5175de286669a149928f65087c7dc0176416a320
Chuck Obaze <chuckobaze@gmail.com>
2015-04-09 03:15:08 -0400

    enqueued stylesheet

---
+++ patti-child/functions.php
@@ -0,0 +1,8 @@
+<?php
+function theme_name_scripts() {
+	wp_enqueue_style( 'style.css', get_stylesheet_uri() );
+	wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
+?
