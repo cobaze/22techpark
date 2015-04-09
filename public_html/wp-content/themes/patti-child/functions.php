9326edc15e0cdc19e19830a90c644bf693641141
Chuck Obaze <chuckobaze@gmail.com>
2015-04-09 11:12:03 -0400

    sass

--- public_html/wp-content/themes/patti-child/functions.php
+++ public_html/wp-content/themes/patti-child/functions.php
@@ -1,9 +1,9 @@
 <?php
 add_action(  'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-	wp_register_style( 'parent-style', get_template_directory_uri() . '/style.css');
+	wp_register_style( 'parent-style', get_template_directory_uri() . 'css/style.css');
 	wp_register_style( 'child-style',
-		get_stylesheet_directory_uri() . '/style.css',
+		get_stylesheet_directory_uri() . 'css/style.css',
 		array('parent-style')
 	);
 	wp_enqueue_style( 'child-style' );
